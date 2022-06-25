6a924184e7cf542d14faf4aea0cdcbfa92a70210
agruezo <agruezo@gmail.com>
2022-06-25 10:02:57 -0400

    add logged in user logic to movie details route

--- flask_app/controllers/movies.py
+++ flask_app/controllers/movies.py
@@ -25,13 +25,17 @@ def dashboard():
 @app.route('/movie/<id>/details')
 def show_movie(id):
 
-    # -- NEED TO ADD LOGIC TO CHECK IF USER IS LOGGED IN -- #
-
+    if 'user_id' not in session:
+        return redirect('/logout')
+    data ={
+        'id': session['user_id']
+    }
     # IMDb API ROUTE THAT RETURNS ONE MOVIE BASED ON ITS IMDb ID
     # PARSED OUT TITLE, PLOT, AND IMDb RATING ON DETAILS.HTML PAGE
     movie = requests.get(f'https://imdb-api.com/en/API/Title/k_19g8uwm0/{id}')
     
     movies = movie.json()
     print(movie.json()['plot'])
+    user=User.get_by_id(data)
 
-    return render_template('movie_details.html', movies = movies)
+    return render_template('movie_details.html', user = user, movies = movies)
