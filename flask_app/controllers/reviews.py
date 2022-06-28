5f16f83c26950532725db0eac06df5e8544f525f
John Doe <johndoe@example.com>
2022-06-28 02:18:08 -0700

    added reviews controller and model, as well as added more to the reviews page

---
+++ flask_app/controllers/reviews.py
@@ -0,0 +1,42 @@
+
+from flask import jsonify, render_template, redirect, session, request
+from flask_app import app
+from flask_app.models.user import User
+from flask_app.models.review import Review
+import requests
+
+
+@app.route('/movies/<id>/reviews')
+def new_review(id):
+    if 'user_id' not in session:
+        return redirect('/logout')
+    data = {'id': session['user_id']}
+    movie = requests.get(f'https://imdb-api.com/en/API/Title/k_19g8uwm0/{id}')
+    movies = movie.json()
+    return render_template('movie_comments.html', users=User.get_by_id(data), movies=movies, reviews=Review.get_all_reviews())
+
+
+@app.route('/create', methods=['POST', 'GET'])
+def create_review():
+    if 'user_id' not in session:
+        return redirect('/logout')
+
+    if not Review.validate_reviews(request.form):
+        return redirect('/movies/<id>/reviews') 
+
+    data = {
+            'rating': request.form['rating'],
+            'comment': request.form['comment'],
+            'user_id': session['user_id'],
+            'movie_id': session['user_id']
+    }
+    Review.save(data)
+    return redirect('/movies/<id>/reviews')
+
+
+@app.route('/delete/<int:id>')
+def delete_job(id):
+    data = {'id': id}
+    Review.delete(data)
+    return redirect('/movies/<id>/reviews')
+    
--- flask_app/models/movie.py
+++ flask_app/models/movie.py
@@ -4,18 +4,3 @@ from flask import flash
 from flask_app.models import user
 
 
-class Review:
-    def __init__(self, data):
-        self.id = data['id']
-        self.rating = data['rating']
-        self.comment = data['comment']
-        self.created_at = data['created_at']
-        self.updated_at = data['updated_at']
-        self.user_id = data['user_id']
-        self.user = None
-
-    @classmethod 
-    def save(cls, data):
-        query = "INSERT INTO reviews (rating, comment, user_id) VALUES (%(rating)s, %(comment)s, %(user_id)s);"
-        return connectToMySQL('popmovie').query_db(query, data)
-
