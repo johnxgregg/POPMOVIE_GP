cda64d653c931cf96bd1a0a399500dbcb73e057c
agruezo <agruezo@gmail.com>
2022-06-29 10:13:30 -0400

    updated movies and reviews controllers

--- flask_app/controllers/movies.py
+++ flask_app/controllers/movies.py
@@ -16,7 +16,7 @@ def dashboard():
     # FREE PLAN ONLY ALLOWS 100 API CALLS/DAY WITH API KEY
     # RETURNS 100 MOST POPULAR MOVIES THAT CAN BE PARSED TO TOP 5 ON DASHBOARD.HTML PAGE
     
-    movie = requests.get(f'https://imdb-api.com/en/API/MostPopularMovies/k_i1e26rt1')
+    movie = requests.get(f'https://imdb-api.com/en/API/MostPopularMovies/k_19g8uwm0')
 
     movies = movie.json()['items']
     print(movie.json()['items'])
@@ -35,7 +35,7 @@ def show_movie(id):
     # IMDb API ROUTE THAT RETURNS ONE MOVIE BASED ON ITS IMDb ID
     # PARSED OUT TITLE, PLOT, AND IMDb RATING ON DETAILS.HTML PAGE
 
-    movie = requests.get(f'https://imdb-api.com/en/API/Title/k_i1e26rt1/{id}')
+    movie = requests.get(f'https://imdb-api.com/en/API/Title/k_19g8uwm0/{id}')
     
     movies = movie.json()
     print(movie.json()['plot'])
--- flask_app/controllers/reviews.py
+++ flask_app/controllers/reviews.py
@@ -13,7 +13,7 @@ def new_review(id):
     data = {'id': session['user_id']}
     movie_data = {'imdb': id}
 
-    movie = requests.get(f'https://imdb-api.com/en/API/Title/k_i1e26rt1/{id}')
+    movie = requests.get(f'https://imdb-api.com/en/API/Title/k_19g8uwm0/{id}')
     movies = movie.json()
     reviews = Review.get_all_reviews(movie_data)
     users = User.get_by_id(data)
